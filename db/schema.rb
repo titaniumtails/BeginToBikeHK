e4dd470b752a32156b6db8ff31ead02881931615
talia scotchbrook <taliaisontheweb@gmail.com>
2014-10-29 21:09:34 +0800

    Generated the Home controller

---
+++ db/schema.rb
@@ -0,0 +1,40 @@
+# encoding: UTF-8
+# This file is auto-generated from the current state of the database. Instead
+# of editing this file, please use the migrations feature of Active Record to
+# incrementally modify your database, and then regenerate this schema definition.
+#
+# Note that this schema.rb definition is the authoritative source for your
+# database schema. If you need to create the application database on another
+# system, you should be using db:schema:load, not running all the migrations
+# from scratch. The latter is a flawed and unsustainable approach (the more migrations
+# you'll amass, the slower it'll run and the greater likelihood for issues).
+#
+# It's strongly recommended that you check this file into your version control system.
+
+ActiveRecord::Schema.define(version: 20141029130548) do
+
+  create_table "bike_routes", force: true do |t|
+    t.string   "name"
+    t.string   "from"
+    t.string   "to"
+    t.string   "from_district"
+    t.string   "to_district"
+    t.string   "map_thumb"
+    t.date     "date"
+    t.text     "map_detail"
+    t.text     "warning"
+    t.string   "warning_thumb"
+    t.text     "description"
+    t.boolean  "mtb"
+    t.integer  "distance"
+    t.time     "time"
+    t.string   "difficulty"
+    t.text     "comments"
+    t.string   "best_tod"
+    t.boolean  "user_generated"
+    t.integer  "ranking"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+end
